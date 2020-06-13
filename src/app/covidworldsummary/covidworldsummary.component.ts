bb8e1a8a257f269fd43b06d7e487eae78d882ca4
Debojyoti Dutta <debojyotidutta94@gmail.com>
2020-06-14 03:13:09 +0530

    dashboard completed (#1)
    Co-authored-by: ddutta2

--- src/app/covidworldsummary/covidworldsummary.component.ts
+++ src/app/covidworldsummary/covidworldsummary.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CovidService } from '../covid.service';
-
+import { formatDate, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-covidworldsummary',
@@ -9,15 +9,27 @@ import { CovidService } from '../covid.service';
 })
 export class CovidworldsummaryComponent implements OnInit {
   products: any;
-  updateddate: string;
-  constructor(private covidAllDataService: CovidService) { }
+  updateddate: any;
+  diff: any;
+  todayDate: any;
+  constructor(private covidAllDataService: CovidService, private datePipe: DatePipe) { }
 
   ngOnInit(): void {
     this.CovidWorldSummaryMethod();
   }
 
-  private CovidWorldSummaryMethod(): void {
+  public CovidWorldSummaryMethod(): void {
     this.covidAllDataService.CovidGetAllDataMethod().
       subscribe(products => this.products = products);
+  }
+
+  GetInterval(): any{
+    this.updateddate = this.datePipe.transform(this.products.updated,'yyyy-MM-dd h:mm:ss'); 
+    this.todayDate = this.datePipe.transform(Date.now(),'yyyy-MM-dd h:mm:ss');
+    this.diff = new Date(this.todayDate).getTime() - new Date(this.updateddate).getTime();    
+    var days = Math.floor(this.diff / (60 * 60 * 24 * 1000));
+    var hours = Math.floor(this.diff / (60 * 60 * 1000)) - (days * 24);
+    var minutes = Math.floor(this.diff / (60 * 1000)) - ((days * 24 * 60) + (hours * 60));
+    return minutes;
   }
 }
